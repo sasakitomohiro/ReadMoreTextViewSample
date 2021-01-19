93a6f73e63289d3485de0ccf29db323be8cdc4a4
sasakitomohiro <t0m0.piii.12.07sk@gmail.com>
2021-01-20 07:09:36 +0900

    add setReadMoreColor

--- app/src/main/java/com/github/sasakitomohiro/readmoretextviewsample/ReadMoreTextView.kt
+++ app/src/main/java/com/github/sasakitomohiro/readmoretextviewsample/ReadMoreTextView.kt
@@ -1,12 +1,15 @@
 package com.github.sasakitomohiro.readmoretextviewsample
 
 import android.content.Context
+import android.graphics.Color
 import android.text.SpannableStringBuilder
 import android.text.Spanned
+import android.text.TextPaint
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.util.AttributeSet
 import android.view.View
+import androidx.annotation.ColorInt
 import androidx.appcompat.widget.AppCompatTextView
 
 class ReadMoreTextView @JvmOverloads constructor(
@@ -19,16 +22,18 @@ class ReadMoreTextView @JvmOverloads constructor(
     defStyle
 ) {
     private var fullText = ""
-    private var trimmedText = ""
+    private var collapsedText = ""
     private var collapseTextCount = 150
     private var readMoreText = "Read More"
     private var isCollapsed = false
+    @ColorInt
+    private var readMoreColor = Color.RED
 
     override fun setText(text: CharSequence?, type: BufferType?) {
         if (isCollapsed) {
-            trimmedText = text.toString().substring(0, collapseTextCount) + "…"
+            collapsedText = text.toString().substring(0, collapseTextCount) + "…"
         }
-        super.setText(if (isCollapsed) setCollapseSpan(trimmedText) else text, type)
+        super.setText(if (isCollapsed) setCollapseSpan(collapsedText) else text, type)
     }
 
     private fun setCollapseSpan(text: String): SpannableStringBuilder {
@@ -40,6 +45,10 @@ class ReadMoreTextView @JvmOverloads constructor(
                     override fun onClick(widget: View) {
                         isCollapsed = false
                         setText(fullText)
+                    }
+
+                    override fun updateDrawState(ds: TextPaint) {
+                        ds.color = readMoreColor
                     }
                 },
                 text.length,
@@ -63,5 +72,9 @@ class ReadMoreTextView @JvmOverloads constructor(
 
     fun setReadMoreText(text: String) {
         readMoreText = text
+    }
+
+    fun setReadMoreColor(@ColorInt color: Int) {
+        readMoreColor = color
     }
 }
